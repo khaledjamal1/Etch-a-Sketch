7d1bb41c552af73311de2287d430d1aad9ecaa89
khaledjamal1 <khalidfaqari@gmail.com>
2022-04-11 13:24:37 +0300

    working grid

--- main.js
+++ main.js
@@ -2,17 +2,22 @@
 
 
 
-// make a grid function row
+// make a grid and add event listner
 const container = document.querySelector(".container")
 const content = document.createElement("div")
 content.id="grid"
 container.appendChild(content)
 function makeDiv(){
-    for(let i =0; i<(16*16); i++){
+    for(let i =0; i<(100*100); i++){
         let div = document.createElement("div")
         div.className = "box"
+        div.setAttribute('draggable', 'false')
+        div.addEventListener("mousedown",() =>{
+            div.style.backgroundColor= "black"
+        })
         document.getElementById("grid").appendChild(div)
     }
+    
 }
 // make a grid column
 makeDiv()
