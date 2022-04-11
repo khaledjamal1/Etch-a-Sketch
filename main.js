e4c9fdc221a43010d898cc3800c714f64052cade
khaledjamal1 <khalidfaqari@gmail.com>
2022-04-11 13:24:23 +0300

    working grid

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
+// user input 
+
+
+
+// make a grid function row
 const container = document.querySelector(".container")
-const square = document.createElement("div")
+const content = document.createElement("div")
+content.id="grid"
+container.appendChild(content)
+function makeDiv(){
+    for(let i =0; i<(16*16); i++){
+        let div = document.createElement("div")
+        div.className = "box"
+        document.getElementById("grid").appendChild(div)
+    }
+}
+// make a grid column
+makeDiv()
