9fa78286232bedda8e281a4c96fef9823d9bd8c9
khaledjamal1 <khalidfaqari@gmail.com>
2022-04-11 16:31:58 +0300

    a working resize

--- main.js
+++ main.js
@@ -4,20 +4,17 @@
 
 // make a grid and add event listner
 const container = document.querySelector(".container")
-const content = document.createElement("div")
-content.id="grid"
-container.appendChild(content)
-function makeDiv(){
-    for(let i =0; i<(100*100); i++){
+function makeDiv(rows, columns){
+    for(let i =0; i<(rows*columns); i++){
         let div = document.createElement("div")
-        div.className = "box"
-        div.setAttribute('draggable', 'false')
-        div.addEventListener("mousedown",() =>{
+        //div.style.gridTemplateRows = `repeat(${rows}, 1fr)`
+        //div.style.gridTemplateColumns = `repeat(${columns}, 1fr)`
+        div.addEventListener("mouseover",() =>{
             div.style.backgroundColor= "black"
         })
-        document.getElementById("grid").appendChild(div)
+        container.appendChild(div).classList.add("box")
     }
     
 }
 // make a grid column
-makeDiv()
+makeDiv(64,64)
